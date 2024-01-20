75ab1ddce5af2509c7d9574db44cd8867645f27b
Yuki Takeyama <namiki.takeyama@gmail.com>
2024-01-20 23:46:30 +0900

    Initial Commit

---
+++ SixDoFSample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SixDoFSample
+//
+//  Created by 武山侑輝 on 2024/01/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SixDoFSample/SixDoFSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SixDoFSampleApp.swift
+//  SixDoFSample
+//
+//  Created by 武山侑輝 on 2024/01/20.
+//
+
+import SwiftUI
+
+@main
+struct SixDoFSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
